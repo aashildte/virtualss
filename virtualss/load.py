384c41d92e912dc29b25e3640d2d3bb78cb7f476
Åshild Telle <aashild@uw.edu>
2023-05-31 14:45:28 -0700

    ++

---
+++ virtualss/load.py
@@ -0,0 +1,55 @@
+
+import dolfin as df
+
+
+def evaluate_normal_load(F, P, mesh, ds, wall_idt):
+    normal_vector = df.FacetNormal(mesh)
+
+    return evaluate_load(F, P, mesh, ds, wall_idt, normal_vector)
+
+def evaluate_shear_load(F, P, mesh, ds, wall_idt):
+    
+    top_dim = mesh.topology().dim()
+
+    unit_vectors = {
+            1: {
+                2 : df.as_vector([-1.0, 0.0]),
+                3 : df.as_vector([-1.0, 0.0, 0.0]),
+                },
+            2: {
+                2 : df.as_vector([1.0, 0.0]),
+                3 : df.as_vector([1.0, 0.0, 0.0]),
+                },
+            3: {
+                2 : df.as_vector([0.0, -1.0]),
+                3 : df.as_vector([0.0, -1.0, 0.0]),
+                },
+            4: {
+                2 : df.as_vector([0.0, 1.0]),
+                3 : df.as_vector([0.0, 1.0, 0.0]),
+                },
+            5: {
+                3 : df.as_vector([0.0, 0.0, -1.0]),
+                },
+            6: {
+                3 : df.as_vector([0.0, 0.0, 1.0]),
+                }
+            }
+
+    unit_vector = unit_vectors[wall_idt][top_dim]
+
+    return evaluate_load(F, P, mesh, ds, wall_idt, unit_vector)
+
+
+def evaluate_load(F, P, mesh, ds, wall_idt, unit_vector):
+    normal_vector = df.FacetNormal(mesh)
+
+    load = df.inner(P * normal_vector, unit_vector)
+    total_load = df.assemble(load * ds(wall_idt))
+    area = df.assemble(         # = total length in 2D
+        df.det(F)
+        * df.inner(df.inv(F).T * unit_vector, unit_vector)
+        * ds(wall_idt)
+    )
+
+    return total_load/area
