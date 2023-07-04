492e39bc680fab3c1929ede553c15ec4aa8439b7
Åshild Telle <aashildte@gmail.com>
2023-07-04 14:03:14 -0700

    updated demos

--- demos/stretch_ff_load.py
+++ demos/stretch_ff_load.py
@@ -11,46 +11,34 @@ import numpy as np
 import dolfin as df
 from mpi4py import MPI
 
-from virtualss import CardiacModel, get_boundary_markers, stretch_ff_load_2D, stretch_ff_load_3D, evaluate_stretch, evaluate_normal_load
+from virtualss import CardiacModel, get_boundary_markers, stretch_xx_load, evaluate_deformation_xdir
 
-mesh_2D = df.UnitSquareMesh(3, 3)
-mesh_3D = df.UnitCubeMesh(3, 3, 3)
+mesh = df.UnitCubeMesh(3, 3, 3)
 
-for mesh, num_free_degrees, stretch_fun in zip(
-    [mesh_2D, mesh_3D], [3, 6], [stretch_ff_load_2D, stretch_ff_load_3D]
-):
+cm = CardiacModel(mesh, remove_rm=True)
 
-    cm = CardiacModel(mesh, num_free_degrees)
+# define weak form terms
+boundary_markers, ds = get_boundary_markers(mesh)
+external_pressure_terms, pressure_fun = stretch_xx_load(cm.F, cm.v, mesh, boundary_markers, ds)
 
-    state, test_state, F = cm.state, cm.test_state, cm.F
-    u, _, _ = state.split()
+# add to weak form
+cm.weak_form += sum(external_pressure_terms)
 
-    # define weak form terms
-    boundary_markers, ds = get_boundary_markers(mesh)
-    Gext, pressure_fun, rm = stretch_fun(state, test_state, F, mesh, boundary_markers, ds)
+# iterate over these values:
+load_values = np.linspace(0, 2.5, 20)
+stretch_values = []
 
-    # add to weak form
-    cm.weak_form += Gext + rm
+# solve problem
+for l in load_values:
+    print(f"Applied load: {l:.2f} kPa")
+    pressure_fun.k = l
 
-    # iterate over these values:
-    load_values = np.linspace(0, 2.5, 20)
-    stretch_values = []
+    cm.solve()
 
-    # solve problem
-    for l in load_values:
-        print(f"Applied load: {l} kPa")
-        pressure_fun.k = l
+    stretch = evaluate_deformation_xdir(cm.u, mesh, boundary_markers, ds)
+    stretch_values.append(100*stretch)
 
-        cm.solve()
-
-        stretch = evaluate_stretch(u, mesh, ds)
-        stretch_values.append(100*stretch)
-        print(stretch)
-        load_eval = evaluate_normal_load(
-            cm.F, cm.P, mesh, ds, 1
-        )
-        load_eval2 = evaluate_normal_load(cm.F, cm.P, mesh, ds, 2)
-        print(l, load_eval, load_eval2)
-
-    plt.plot(stretch_values, load_values)
+plt.plot(stretch_values, load_values)
+plt.xlabel("Stretch (%)")
+plt.ylabel("Load (kPa)")
 plt.show()
--- demos/stretch_ff_xcomp.py
+++ demos/stretch_ff_xcomp.py
@@ -13,57 +13,44 @@ from mpi4py import MPI
 
 from virtualss import (
     CardiacModel,
-    evaluate_stretch,
     get_boundary_markers,
-    get_length,
-    stretch_ff_xcomp_2D,
-    stretch_ff_xcomp_3D,
+    stretch_xx_xcomp,
     evaluate_normal_load,
 )
 
 # define mesh and cardiac mechanics
-mesh_2D = df.UnitSquareMesh(3, 3)
-mesh_3D = df.UnitCubeMesh(3, 3, 3)
+mesh = df.UnitCubeMesh(3, 3, 3)
+cm = CardiacModel(mesh)
 
+state, test_state, F = cm.state, cm.test_state, cm.F
+u, _ = state.split()
+V = cm.state_space.sub(0)
 
-for mesh, stretch_fun in zip(
-    [mesh_2D, mesh_3D], [stretch_ff_xcomp_2D, stretch_ff_xcomp_3D]
-):
+# boundary markers
+boundary_markers, ds = get_boundary_markers(mesh)
+wall_idt = boundary_markers["xmax"]["idt"]
 
-    num_free_degrees = 0
-    cm = CardiacModel(mesh, num_free_degrees)
+# define weak form terms
+bcs, bcsfun = stretch_xx_xcomp(V, boundary_markers)
 
-    state, test_state, F = cm.state, cm.test_state, cm.F
-    u, _ = state.split()
-    V = cm.state_space.sub(0)
+# iterate over these values:
+stretch_values = np.linspace(0, 0.122, 10)
+load_values = []
 
-    # boundary markers
-    L = get_length(mesh)
-    boundary_markers, ds = get_boundary_markers(mesh)
+cm.solve(bcs=bcs)
 
-    # define weak form terms
-    bcs, bcsfun = stretch_fun(L, V, boundary_markers, mesh)
-
-    # iterate over these values:
-    stretch_values = np.linspace(0, 0.122, 10)
-    load_values = []
+# solve problem
+for i, s in enumerate(stretch_values):
+    print(f"Applied stretch: {100*s:.2f} %")
+    bcsfun.k = s
 
     cm.solve(bcs=bcs)
 
-    # solve problem
-    for i, s in enumerate(stretch_values):
-        print(f"Applied stretch: {100*s} %")
-        bcsfun.k = s
-
-        cm.solve(bcs=bcs)
-
-        load = evaluate_normal_load(
-            cm.F, cm.P, mesh, ds, 1
-        ) + evaluate_normal_load(cm.F, cm.P, mesh, ds, 2)
-        load_values.append(load)
+    load = evaluate_normal_load(cm.F, cm.P, mesh, ds, wall_idt)
+    load_values.append(load)
 
 
-    plt.plot(100*np.array(stretch_values), load_values)
+plt.plot(100*np.array(stretch_values), load_values)
 
 plt.xlabel("Stretch (%)")
 plt.ylabel("Load (kPa)")
