78944511c6adbed2a80ce80d99ab182f603233d7
Åshild TElle <aashild@uw.edu>
2024-04-30 16:33:40 -0700

    projection for load, removes numerical artifacts

--- virtualss/load.py
+++ virtualss/load.py
@@ -11,12 +11,45 @@ def external_pressure_term(external_pressure_fun, F, v, mesh, ds, dir_vector=Non
     return external_pressure_fun* df.inner(v, df.det(F) * df.inv(F) * dir_vector) * ds
 
 
-def evaluate_normal_load(F, P, mesh, ds, wall_idt):
-    normal_vector = df.FacetNormal(mesh)
-    return evaluate_load(F, P, mesh, ds, wall_idt, normal_vector)
+def _get_normal_vector(wall_idt, dim):
+    # this doesn't work with the projection, but the point is to resemble this operation:
+    # normal_vector = df.FacetNormal(mesh)
+    
+    normal_vectors = {
+            1: {
+                2 : df.as_vector([1.0, 0.0]),
+                3 : df.as_vector([1.0, 0.0, 0.0]),
+                },
+            2: {
+                2 : df.as_vector([-1.0, 0.0]),
+                3 : df.as_vector([-1.0, 0.0, 0.0]),
+                },
+            3: {
+                2 : df.as_vector([0.0, 1.0]),
+                3 : df.as_vector([0.0, 1.0, 0.0]),
+                },
+            4: {
+                2 : df.as_vector([0.0, -1.0]),
+                3 : df.as_vector([0.0, -1.0, 0.0]),
+                },
+            5: {
+                3 : df.as_vector([0.0, 0.0, 1.0]),
+                },
+            6: {
+                3 : df.as_vector([0.0, 0.0, -1.0]),
+                },
+            }
+
+    return normal_vectors[wall_idt][dim]
+
+def evaluate_normal_load(F, P, CG, mesh, ds, wall_idt):
+    top_dim = mesh.topology().dim()
+    
+    normal_vector = _get_normal_vector(wall_idt, top_dim)
+    return evaluate_load(F, P, CG, mesh, ds, wall_idt, normal_vector, normal_vector)
 
 
-def evaluate_shear_load(F, P, mesh, ds, wall_idt, direction):    
+def evaluate_shear_load(F, P, CG, mesh, ds, wall_idt, direction):    
     top_dim = mesh.topology().dim()
 
     unit_vectors = {
@@ -32,19 +65,20 @@ def evaluate_shear_load(F, P, mesh, ds, wall_idt, direction):
                 3 : df.as_vector([0.0, 0.0, 1.0]),
                 },
             }
-
+    
     unit_vector = unit_vectors[direction][top_dim]
+    normal_vector = _get_normal_vector(wall_idt, top_dim)
 
-    return evaluate_load(F, P, mesh, ds, wall_idt, unit_vector)
+    return evaluate_load(F, P, CG, mesh, ds, wall_idt, unit_vector, normal_vector)
 
 
-def evaluate_load(F, P, mesh, ds, wall_idt, unit_vector):
-    normal_vector = df.FacetNormal(mesh)
+def evaluate_load(F, P, CG, mesh, ds, wall_idt, normal_vector, unit_vector):
+    
+    load = df.project(df.inner(P * normal_vector, unit_vector), CG)
 
-    load = df.inner(P * normal_vector, unit_vector)
     total_load = df.assemble(load * ds(wall_idt))
 
-    area = df.assemble(         # = total length in 2D
+    area = df.assemble(         # length in 2D
         df.det(F)
         * df.inner(df.inv(F).T * unit_vector, unit_vector)
         * ds(wall_idt)
