aaa2cf31488f8afcf6f710438c5c57705a4b9170
Åshild TElle <aashild@uw.edu>
2024-06-27 16:35:21 -0700

    50% increase instead

--- invertion/inverse_parameter_estimation.py
+++ invertion/inverse_parameter_estimation.py
@@ -39,6 +39,8 @@ def fiber_dir_stretch(mesh, material_parameters, stretch_goal):
 
     cm.solve(bcs=bcs)
 
+    U = df.FunctionSpace(mesh, "CG", 2)
+
     # solve problem
     for i, s in enumerate(stretch_values):
         print(f"Applied stretch: {100*s:.2f} %")
@@ -46,7 +48,7 @@ def fiber_dir_stretch(mesh, material_parameters, stretch_goal):
 
         cm.solve(bcs=bcs)
 
-        load = evaluate_normal_load(cm.F, cm.P, mesh, ds, wall_idt)
+        load = evaluate_normal_load(cm.F, cm.P, U, mesh, ds, wall_idt)
         load_values.append(load)
 
     return stretch_values, load_values
@@ -64,6 +66,7 @@ def sheet_dir_stretch(mesh, material_parameters, stretch_goal):
 
     # define weak form terms
     bcs, bcsfun = stretch_yy_comp(V, boundary_markers)
+    U = df.FunctionSpace(mesh, "CG", 2)
 
     # iterate over these values:
     stretch_values = np.linspace(0, stretch_goal, 20)
@@ -78,24 +81,25 @@ def sheet_dir_stretch(mesh, material_parameters, stretch_goal):
 
         cm.solve(bcs=bcs)
 
-        load = evaluate_normal_load(cm.F, cm.P, mesh, ds, wall_idt)
+        load = evaluate_normal_load(cm.F, cm.P, U, mesh, ds, wall_idt)
         load_values.append(load)
 
     return stretch_values, load_values
 
+import sys
 
 # set parameters for either fiber or transverse direction stretch here!
 fiber_dir = 1  # 0 for sheet/transverse direction stretch
 
 if fiber_dir == 1:
-    a_scaling = [1.0, 1.0, 1.0]
-    af_scaling = [0.715, 1.0, 1.285]  # values found by manual adjustmnet
+    a_scaling = [1.0, 1.0]
+    af_scaling = [1.0, 2.1411]  # values found by manual adjustment
     alpha_fiber = 1
     alpha_transv = 0.2
     filename = "fiber_direction_stretch_pertubration.png"
 else:
-    a_scaling = [0.75, 1.0, 1.25]  # values found by manual adjustment
-    af_scaling = [1.0355, 1.0, 0.9645]  # values found by manual adjustment
+    a_scaling = [1.0, 2.0]       # values found by manual adjustment
+    af_scaling = [1.0, 0.8587]      # values found by manual adjustment
     alpha_fiber = 0.2
     alpha_transv = 1
     filename = "transverse_direction_stretch_pertubration.png"
@@ -112,7 +116,6 @@ colors = ["tab:blue", "darkgray", "tab:red"]
 
 for sv1, sv2, color in zip(a_scaling, af_scaling, colors):
     material_parameters = {"a": sv1 * 2.92, "b": 5.60, "a_f": sv2 * 11.84, "b_f": 17.95}
-    print(material_parameters)
 
     # fiber dir
     stretch_values, load_values = fiber_dir_stretch(
@@ -179,19 +182,19 @@ plt.legend(
     ]
 )
 
-l1, l2, l3 = fiber_loads_final
+l1, l2 = fiber_loads_final
 print("Load difference in fiber direction stretch: ")
 print(
-    f"* Decrease: {round((l2 - l1)/l2*100, 2)}%, increase: {round((l3 - l2)/l2*100,2)}%"
+    f"* Increase: {round((l2 - l1)/l2*100, 2)}%"
 )
 
-l1, l2, l3 = sheet_loads_final
+l1, l2 = sheet_loads_final
 print("Load difference in transverse direction stretch: ")
 print(
-    f"* Decrease: {round((l2 - l1)/l2*100, 2)}%, increase: {round((l3 - l2)/l2*100,2)}%"
+    f"* Increase: {round((l2 - l1)/l2*100, 2)}%"
 )
 
-
+exit()
 plt.xlabel("Stretch (%)")
 plt.ylabel("Load (kPa)")
 
