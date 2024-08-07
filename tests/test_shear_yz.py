dc6d3cc49958de2e684e9795fbcff6f6adca66e0
Åshild TElle <aashild@uw.edu>
2024-08-07 11:39:43 -0700

    +yz / zy directions

--- tests/test_shear_yz.py
+++ tests/test_shear_yz.py
@@ -1,6 +1,6 @@
 """
 
-Unit tests for the code in the "shear_xy" file, testing shear functions
+Unit tests for the code in the "shear_yz" file, testing shear functions
 by assessing whether resulting shear/load matches applied values.
 
 Åshild Telle / University of Washington / 2024
@@ -14,40 +14,34 @@ import pytest
 from virtualss import (
     CardiacModel,
     get_boundary_markers,
-    simple_shear_yx,
+    simple_shear_yz,
     evaluate_normal_load,
-    evaluate_deformation_xmax,
     evaluate_deformation_ymax,
     evaluate_deformation_zmax,
 )
 
 
-@pytest.mark.parametrize("dim", [2, 3])
-def test_simple_shear_xy(dim):
+def test_simple_shear_yz():
     N = 3
-
-    if dim == 2:
-        mesh = df.UnitSquareMesh(N, N)
-    else:
-        mesh = df.UnitCubeMesh(N, N, N)
+    mesh = df.UnitCubeMesh(N, N, N)
 
     cm = CardiacModel(mesh)
 
     boundary_markers, ds = get_boundary_markers(mesh)
-    bcs, bcsfun = simple_shear_yx(cm.V, boundary_markers)
+    bcs, bcsfun = simple_shear_yz(cm.V, boundary_markers)
 
     assigned_shear = 0.05
     bcsfun.k = assigned_shear
 
     cm.solve(bcs)
 
-    shear_xdir = evaluate_deformation_xmax(cm.u, mesh, boundary_markers, ds)
     shear_ydir = evaluate_deformation_ymax(cm.u, mesh, boundary_markers, ds)
-
+    shear_zdir = evaluate_deformation_zmax(cm.u, mesh, boundary_markers, ds)
+ 
     assert np.isclose(
-        assigned_shear / 2, shear_xdir, rtol=0.05
-    ), f"Error: Assigned shear: {assigned_shear / 2} != resulting shear: {shear_xdir}"
+        assigned_shear / 2, shear_zdir, rtol=0.05
+    ), f"Error: Assigned shear: {assigned_shear / 2} != resulting shear: {shear_ydir}"
 
     assert np.isclose(
         shear_ydir, 0.0
-    ), "Error: Domain not fixed in perpendicular direction" 
+    ), "Error: Domain not fixed in perpendicular direction"
---
+++ tests/test_shear_zy.py
@@ -0,0 +1,47 @@
+"""
+
+Unit tests for the code in the "shear_zy" file, testing shear functions
+by assessing whether resulting shear/load matches applied values.
+
+Åshild Telle / University of Washington / 2024
+
+"""
+
+import dolfin as df
+import numpy as np
+import pytest
+
+from virtualss import (
+    CardiacModel,
+    get_boundary_markers,
+    simple_shear_zy,
+    evaluate_normal_load,
+    evaluate_deformation_ymax,
+    evaluate_deformation_zmax,
+)
+
+
+def test_simple_shear_zy():
+    N = 3
+    mesh = df.UnitCubeMesh(N, N, N)
+
+    cm = CardiacModel(mesh)
+
+    boundary_markers, ds = get_boundary_markers(mesh)
+    bcs, bcsfun = simple_shear_zy(cm.V, boundary_markers)
+
+    assigned_shear = 0.05
+    bcsfun.k = assigned_shear
+
+    cm.solve(bcs)
+
+    shear_ydir = evaluate_deformation_ymax(cm.u, mesh, boundary_markers, ds)
+    shear_zdir = evaluate_deformation_zmax(cm.u, mesh, boundary_markers, ds)
+ 
+    assert np.isclose(
+        assigned_shear / 2, shear_ydir, rtol=0.05
+    ), f"Error: Assigned shear: {assigned_shear / 2} != resulting shear: {shear_ydir}"
+
+    assert np.isclose(
+        shear_zdir, 0.0
+    ), "Error: Domain not fixed in perpendicular direction"
